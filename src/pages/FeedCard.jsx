a6c9df19130491dcd679639fc5d52022ebd35e4b
nirajkumardangi <niraj8825224435@gmail.com>
2026-01-19 00:15:38 +0530

    feat: now feed work properly like tinder swipe and also swipe with left and right key

--- src/pages/FeedCard.jsx
+++ src/pages/FeedCard.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+
 import { CheckCircle2 } from "lucide-react";
 import { FaTimes, FaHeart } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
 import SkillTags from "../components/SkillTags";
 
-function FeedCard({ user }) {
-  const { name, avatar, about, headline, location, skills } = user;
+function FeedCard({ user, handleDislike, handleConnect }) {
+  const data = useSelector((s) => s.feed);
+
+  const { _id, name, avatar, about, headline, location, skills } = user;
+
+  const city = location?.city || "";
+  const country = location?.country || "";
+
+  const handleKey = useCallback(
+    (e) => {
+      if (!data.length) return;
+
+      if (!data.length) return;
+      if (e.key === "ArrowLeft") handleDislike(_id);
+      if (e.key === "ArrowRight") handleConnect(_id);
+    },
+    [data.length, _id, handleDislike, handleConnect],
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKey);
+    return () => window.removeEventListener("keydown", handleKey);
+  }, [handleKey]);
 
   return (
-    <div className="relative mb-10 w-full max-w-96 bg-slate-800 rounded-3xl shadow-2xl overflow-hidden border border-slate-700">
-      <div className="relative h-80 w-full">
+    <div className="relative mt-2 w-full max-w-90 md:max-w-86 bg-slate-800 rounded-3xl shadow-2xl overflow-hidden border border-slate-700">
+      <div className="relative h-62 w-full">
         <img
           src={avatar}
           alt={`${name} profile picture png`}
@@ -28,24 +52,26 @@ function FeedCard({ user }) {
           <p className="text-pink-400 text-1xl font-medium">{headline}</p>
           {/* --- Location --- */}
           <div className="flex items-center gap-1 my-1 text-sm text-gray-400">
-            <FaLocationDot /> {`${location.city}, ${location.country}`}
+            <FaLocationDot />
+            {city && country ? `${city}, ${country}` : "Location not available"}
           </div>
         </div>
       </div>
 
       <div className="py-3 px-6">
-        <p className="text-slate-400 text-sm font-medium y-2 line-clamp-3">
+        <p className="text-slate-400 text-sm font-medium my-2 line-clamp-3">
           {about}
         </p>
 
         {/* --- Colorfull Skills Tags --- */}
         <div className="mt-3">
-          <SkillTags skills={skills} size="large" />
+          {skills?.length > 0 && <SkillTags skills={skills} size="large" />}
         </div>
 
         {/* --- Action Buttons --- */}
         <div className="flex justify-center items-center gap-6 mt-6 mb-4">
           <button
+            onClick={() => handleDislike(_id)}
             className="w-16 h-16 bg-gray-800 rounded-full flex items-center justify-center hover:bg-red-500/10 hover:border-red-500 border-2 border-gray-700 transition-all group shadow-lg shadow-black/40 cursor-pointer"
             aria-label="Pass"
           >
@@ -53,6 +79,7 @@ function FeedCard({ user }) {
           </button>
 
           <button
+            onClick={() => handleConnect(_id)}
             className="w-16 h-16 bg-gray-800 rounded-full flex items-center justify-center hover:bg-green-500/10 hover:border-green-500 border-2 border-gray-700 transition-all group shadow-lg shadow-black/40 cursor-pointer"
             aria-label="Connect"
           >
